72fa325eab4b294add851722856946cbe68e9bfe
ab192130 <5365476+ab192130@users.noreply.github.com>
2021-04-25 00:41:13 +0400

    added login page.

--- app/main/ui/textfield.js
+++ app/main/ui/textfield.js
@@ -1,10 +1,10 @@
-const Textfield = ({name, placeholder, label}) => {
+const Textfield = ({name, placeholder, label, type}) => {
     return (
         <div className="relative">
             <label htmlFor={name} className="text-gray-700">
                 {label}
             </label>
-            <input type="text" id={name}
+            <input type={type} id={name}
                    className="rounded-xl border-transparent flex-1 appearance-none border border-gray-300 w-full
                    py-4 px-6 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none
                    focus:ring mt-2 focus:ring-blue-600 focus:border-transparent focus:shadow-xl transition ease-in-out"
@@ -12,6 +12,10 @@ const Textfield = ({name, placeholder, label}) => {
                    name={name} placeholder={placeholder}/>
         </div>
     );
+};
+
+Textfield.defaultProps = {
+    type: 'text'
 };
 
 export default Textfield;
---
+++ pages/login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginLayout from "../layouts/login";
+
+const Login = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+Login.Layout = LoginLayout;
+
+export default Login;
