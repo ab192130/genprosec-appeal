2c3fc2710fa292e799a14586ae9f321fb5e89862
ab192130 <5365476+ab192130@users.noreply.github.com>
2021-04-24 22:44:07 +0400

    minor changes.

--- app/main/ui/textfield.js
+++ app/main/ui/textfield.js
@@ -7,7 +7,7 @@ const Textfield = ({name, placeholder, label}) => {
             <input type="text" id={name}
                    className="rounded-xl border-transparent flex-1 appearance-none border border-gray-300 w-full
                    py-4 px-6 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none
-                   focus:ring-2 focus:ring-blue-600 focus:border-transparent focus:shadow-xl transition ease-in-out"
+                   focus:ring mt-2 focus:ring-blue-600 focus:border-transparent focus:shadow-xl transition ease-in-out"
                    autoComplete="off"
                    name={name} placeholder={placeholder}/>
         </div>
