7739d70272f0ef9cc83a12d53785ae59e6e9393d
ab192130 <5365476+ab192130@users.noreply.github.com>
2021-04-24 17:42:23 +0400

    minor changes.

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,32 @@
 import Head from 'next/head'
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Onlayn müraciətlər</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <Head>
+                <title>Onlayn müraciətlər</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
 
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        [məzmun]
-      </main>
+            <header>
+                [başlıq]
+            </header>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Made by{' '}
-          <img src="/logo.svg" alt="Made by Burhan Aghazada" className="h-12 ml-2" />
-        </a>
-      </footer>
-    </div>
-  )
+            <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
+                [məzmun]
+            </main>
+
+            <footer className="flex items-center justify-center w-full h-24 border-t">
+                <a
+                    className="flex items-center justify-center"
+                    href="/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Made by{' '}
+                    <img src="/logo.svg" alt="Made by Burhan Aghazada" className="h-12 ml-2"/>
+                </a>
+            </footer>
+        </div>
+    )
 }
