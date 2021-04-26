37aa61c9f20b23ee16a24e99435fd6d5fe458a44
ab192130 <5365476+ab192130@users.noreply.github.com>
2021-04-26 13:40:40 +0400

    minor changes.

--- pages/about.js
+++ pages/about.js
@@ -40,14 +40,8 @@ const About = () => {
                         className="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 px-6 py-4 bg-white mt-6 shadow-lg rounded-3xl dark:bg-gray-800">
                         <div className="flex-shrink-0">
                             <div
-                                className="flex items-center mx-auto rounded-full justify-center h-12 w-12 rounded-md bg-blue-600 text-white">
-                                <svg width="20" height="20" fill="currentColor" className="h-6 w-6"
-                                     viewBox="0 0 1792 1792"
-                                     xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
-                                    </path>
-                                </svg>
+                                className="flex items-center mx-auto rounded-full justify-center h-12 w-12 rounded-md bg-blue-50 text-white">
+                                <img src="/logo_react.svg" className="h-6 w-6" alt=""/>
                             </div>
                         </div>
                         <h3 className="text-2xl sm:text-xl text-gray-700 font-semibold dark:text-white py-4">
@@ -63,14 +57,8 @@ const About = () => {
                         className="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 px-4 py-4 mt-6 sm:mt-16 md:mt-20 lg:mt-24 bg-white shadow-lg rounded-3xl dark:bg-gray-800">
                         <div className="flex-shrink-0">
                             <div
-                                className="flex items-center mx-auto rounded-full justify-center h-12 w-12 rounded-md bg-blue-600 text-white">
-                                <svg width="20" height="20" fill="currentColor" className="h-6 w-6"
-                                     viewBox="0 0 1792 1792"
-                                     xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
-                                    </path>
-                                </svg>
+                                className="flex items-center mx-auto rounded-full justify-center h-12 w-12 rounded-md bg-green-50 text-white">
+                                <img src="/logo_nodejs.svg" className="h-6 w-6" alt=""/>
                             </div>
                         </div>
                         <h3 className="text-2xl sm:text-xl text-gray-700 font-semibold dark:text-white py-4">
@@ -85,14 +73,8 @@ const About = () => {
                         className="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 mt-6  px-4 py-4 bg-white shadow-lg rounded-3xl dark:bg-gray-800">
                         <div className="flex-shrink-0">
                             <div
-                                className="flex items-center mx-auto rounded-full justify-center h-12 w-12 rounded-md bg-blue-600 text-white">
-                                <svg width="20" height="20" fill="currentColor" className="h-6 w-6"
-                                     viewBox="0 0 1792 1792"
-                                     xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
-                                    </path>
-                                </svg>
+                                className="flex items-center mx-auto rounded-full justify-center h-12 w-12 rounded-md bg-purple-50 text-white">
+                                <img src="/logo_api.svg" className="h-6 w-6" alt=""/>
                             </div>
                         </div>
                         <h3 className="text-2xl sm:text-xl text-gray-700 font-semibold dark:text-white py-4">
