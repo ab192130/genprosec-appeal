664824dd4b01c94ca4d195d12ea0981ec3c9cff5
ab192130 <5365476+ab192130@users.noreply.github.com>
2021-04-26 09:09:46 +0400

    minor changes.

--- app/system/ui/list-item.js
+++ app/system/ui/list-item.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Link from "next/link";
+import {useRouter} from "next/router";
 
 const ListItem = ({children, to}) => {
+    const router = useRouter();
+
     return (
         <Link href={to}>
-            <a className="hover:text-blue-600 text-gray-600 bg-gray-50 dark:bg-gray-600 dark:text-gray-400
+            <a className={`${router.pathname === to ? "bg-blue-100 text-blue-600 hover:text-blue-200" : ''} hover:text-blue-600 text-gray-600 bg-gray-50 dark:bg-gray-600 dark:text-gray-400
                         hover:bg-blue-100 flex items-center p-2 my-6 transition-colors dark:hover:text-white
-                        dark:hover:bg-gray-600 duration-200 justify-start active:bg-blue-200 cursor-pointer rounded-xl">
+                        dark:hover:bg-gray-600 duration-200 justify-start active:bg-blue-200 cursor-pointer rounded-xl`}>
                 {children}
             </a>
         </Link>
