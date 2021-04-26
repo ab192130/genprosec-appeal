bc7ccfe3d6e020fc2f39a9a1514517cff7fc81c1
ab192130 <5365476+ab192130@users.noreply.github.com>
2021-04-26 12:45:30 +0400

    minor changes.

--- app/system/partials/sidebar.js
+++ app/system/partials/sidebar.js
@@ -90,6 +90,21 @@ const Sidebar = () => {
                             </ListItem>
                         </div>
                     </nav>
+
+                    <div className="absolute bottom-0 my-10">
+                        <Link href="/">
+                            <a className="text-gray-300 dark:text-gray-300 hover:text-gray-800 dark:hover:text-gray-100 transition-colors duration-200 flex items-center py-2 px-8">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                     stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                          d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"/>
+                                </svg>
+                                <span className="mx-4 font-medium">
+                                    Çıxış
+                                </span>
+                            </a>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
