b8425f4925772f84aea4f03ed17a3930d96987e6
ab192130 <5365476+ab192130@users.noreply.github.com>
2021-04-26 13:19:33 +0400

    minor changes.

--- pages/submit.js
+++ pages/submit.js
@@ -141,7 +141,7 @@ const Submit = () => {
                     <div className="mt-10">
                         <div className="text-7xl mb-10">
                             <div className="font-bold mb-5"><span className="text-blue-600">Kim</span>siniz?</div>
-                            <div className="text-xl text-gray-400">Özünüzw təqdim edin</div>
+                            <div className="text-xl text-gray-400">Özünüzü təqdim edin</div>
                         </div>
 
                         <div className="grid sm:grid-cols-2 grid-cols-1 gap-10 mb-10 p-10">
